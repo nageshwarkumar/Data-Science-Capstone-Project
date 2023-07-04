13eecd559327334c95616841195bda0721598540
nageshwarkumar <137165318+nageshwarkumar@users.noreply.github.com>
2023-07-04 12:24:14 +0530

    app.py

--- app.py
+++ app.py
@@ -1,60 +1,74 @@
-import pandas as pd
 import streamlit as st
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+import numpy as np
+from PIL import Image
+import pickle
+import requests
 
-# Load the dataset
-car_data = pd.read_csv('CAR DETAILS.csv')
-
-# Select relevant features and target variable
-X = car_data[['Brand', 'year', 'km_driven', 'fuel', 'seller_type', 'transmission', 'owner']]
-y = car_data['selling_price']
-
-# Perform label encoding for categorical variables
-label_encoder = LabelEncoder()
-X['Brand'] = label_encoder.fit_transform(X['Brand'])
-X['fuel'] = label_encoder.fit_transform(X['fuel'])
-X['seller_type'] = label_encoder.fit_transform(X['seller_type'])
-X['transmission'] = label_encoder.fit_transform(X['transmission'])
-X['owner'] = label_encoder.fit_transform(X['owner'])
-
-# Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Create and train the Random Forest regression model
-model = RandomForestRegressor()
-model.fit(X_train, y_train)
-
-# Define the Streamlit app
 def main():
-    st.title('Car Price Prediction')
-    st.markdown('Enter the details of the car to get the predicted price.')
+    # Categorical inputs
 
-    # Collect input from the user
-    brand = st.selectbox('Brand', car_data['Brand'].unique())
-    year = st.number_input('Year')
-    km_driven = st.number_input('Kilometers Driven')
-    fuel = st.selectbox('Fuel', car_data['fuel'].unique())
-    seller_type = st.selectbox('Seller Type', car_data['seller_type'].unique())
-    transmission = st.selectbox('Transmission', car_data['transmission'].unique())
-    owner = st.selectbox('Owner', car_data['owner'].unique())
+    image = Image.open('logo.png')
+    st.title("Car_Price_Predictor")
 
-    # Encode the input
-    brand_encoded = label_encoder.transform([brand])
-    fuel_encoded = label_encoder.transform([fuel])
-    seller_type_encoded = label_encoder.transform([seller_type])
-    transmission_encoded = label_encoder.transform([transmission])
-    owner_encoded = label_encoder.transform([owner])
+    st.sidebar.image(image, caption=f"Suri Enterprises Pvt. Ltd.", use_column_width=True)
+    st.sidebar.subheader("Welcome to The Suri Enterprises Pvt. Ltd !")
 
-    # Make prediction on the input
-    new_car = [[brand_encoded[0], year, km_driven, fuel_encoded[0], seller_type_encoded[0], transmission_encoded[0], owner_encoded[0]]]
-    predicted_price = model.predict(new_car)
+    st.sidebar.subheader(
+        "We rent and sales cars. This company is based on second hand or branded new cars. All the facility are available here.")
+    st.sidebar.write("Ravi Ranjan ", "\n", "Chairman & CEO")
+    st.sidebar.subheader('Contact Us.  \n'
+                         'Email:-  ranjeet.suri241@gmail.com')
 
-    # Display the predicted price
-    st.subheader('Predicted Price')
-    st.write(predicted_price)
+    st.sidebar.subheader("+91 75491 19745")
 
-if __name__ == '__main__':
+    owner1 = {"First Owner": 1, "Second Owner": 2, "Third Owner": 3, "Fourth Owner and Above Owner": 4,
+              "Test Drive Car": 5}
+    seller1 = {"Individual": 1, "Dealer": 2, "Trust mark Dealer": 3}
+    transmission1 = {"Manual": 1, "Automatic": 2}
+    brand1 = {"Maruti": 1, "Hyundai": 2, "Mahindra": 3, "Tata": 4, "Honda": 5, "Ford": 6, "Toyota": 7, "Chevrolet": 8,
+              "Renault": 9, "Volkswagen": 10,
+              "Skoda": 11, "Nissan": 12, "Audi": 13, "BMW": 14, "Fiat": 15, "Datsun": 16, "Mercedes-Benz": 17,
+              "Jaguar": 18, "Mitsubishi": 19, "Land": 20,
+              "Volvo": 21, "Ambassador": 22, "Jeep": 23, "MG": 24, "OpelCorsa": 25, "Daewoo": 26, "Force": 27,
+              "Isuzu": 28, "Kia ": 29}
+    engine1 = {"Diesel": 1, "Patrol": 2, "CNG": 3, "LPG": 4,"Electric": 5}
+
+    name = st.text_input("Enter Car model Name")
+    brand = st.selectbox("Brand", tuple(brand1.keys()))
+    year = st.number_input("Year of purchase", 1900, 2023)
+    driver = st.number_input("Driver(KM)")
+    owner_type = st.selectbox("Owner Type", tuple(owner1.keys()))
+    engine_type = st.selectbox("Engine type", tuple(engine1.keys()))
+    transmission_type = st.selectbox("Transmission", tuple(transmission1.keys()))
+    seller_type = st.selectbox("Seller type", tuple(seller1.keys()))
+
+    def get_value(val, my_dict):
+        for key, value in my_dict.items():
+            if val == key:
+                return value
+
+    def load_model(model_file):
+        model = pickle.load(open(model_file, "rb"))
+        return model
+
+    if st.button("Predict"):
+        feature_list = [get_value(brand, brand1), int(year), int(driver), get_value(owner_type, owner1),
+                        get_value(engine_type, engine1), get_value(transmission_type, transmission1),
+                        get_value(seller_type, seller1)]
+        # st.write(feature_list)
+        st.subheader("Your Input")
+        user_input_data = {"Name": name, "Brand": brand, "Year of purchase": year, "Drive(KM)": driver,
+                           "Owner_Type": owner_type, "Engine Type": engine_type, "Transmission Type": transmission_type,
+                           "Seller Type": seller_type}
+        st.write(user_input_data)
+        st.subheader("Predicted Selling Price")
+        input_data = np.array(feature_list).reshape(1, -1)
+        model =load_model("final_model.pkl")
+        prediction = model.predict(input_data)
+        st.write("Predicted Selling Price :" + " " + "₹" +" " + str(np.round(prediction[0], 2)))
+
+        st.subheader(''' Thank you for your visit !''')
+
+
+if __name__ == "__main__":
     main()
